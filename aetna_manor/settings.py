17b13c7c66828d64f7f321f7f66da3bde911792f
brysonmckenzie <brysonmckenzie83@gmail.com>
2018-05-29 07:23:48 +0000

    full-web

--- aetna_manor/settings.py
+++ aetna_manor/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l(s92*@ijzqxf9v9cjxrhm!#neqnm=9(ar!yk%-4#+$+94fqkh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['204.48.24.124']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
