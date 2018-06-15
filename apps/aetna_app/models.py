2824d8cb8724bda90d47b40d0fffd0c50f8a3a3f
brysonmckenzie <brysonmckenzie83@gmail.com>
2018-06-15 13:17:49 -0500

    mobile responsiveness updates

--- apps/aetna_app/models.py
+++ apps/aetna_app/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    birthdate = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    address = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now = True)
